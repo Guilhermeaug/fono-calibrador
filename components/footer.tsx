86728160ed11372f7abf5c6c9333b8b4e44c49a5
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2024-07-29 20:26:33 -0300

    Update footer styles and images

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,53 +2,53 @@ import Image from "next/image";
 
 export function Footer() {
   return (
-    <footer className="grid items-center grid-cols-3 space-x-4 px-4 md:grid-cols-6 md:px-8">
+    <footer className="grid grid-cols-3 items-center space-x-4 px-4 md:grid-cols-6 md:px-8">
       <div className="relative h-12">
         <Image
+          className="object-contain"
           src="/images/medicina.png"
           alt="Medicina"
           layout="fill"
-          objectFit="contain"
         />
       </div>
       <div className="relative h-12">
         <Image
+          className="object-contain"
           src="/images/ufmg.png"
           alt="UFMG"
           layout="fill"
-          objectFit="contain"
         />
       </div>
-      <div className="relative h-14">
+      <div className="relative h-16">
         <Image
+          className="object-contain"
           src="/images/gp-voz.png"
           alt="GP Voz"
           layout="fill"
-          objectFit="contain"
         />
       </div>
       <div className="relative h-12">
         <Image
+          className="object-contain"
           src="/images/cnpq.png"
           alt="CNPq"
           layout="fill"
-          objectFit="contain"
         />
       </div>
       <div className="relative h-12">
         <Image
+          className="object-contain"
           src="/images/capes.png"
           alt="CAPES"
           layout="fill"
-          objectFit="contain"
         />
       </div>
       <div className="relative h-12">
         <Image
+          className="object-contain"
           src="/images/audbility.png"
           alt="Audbility"
           layout="fill"
-          objectFit="contain"
         />
       </div>
     </footer>
